7fcae2e1f15650cd7af3ea3642f62dbe4489e551
Alex Geovanni <A180065@unach.mx>
2023-05-09 22:46:21 -0600

    hasta aqui funciona

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ import jsonServer from "json-server"
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000;
-
 
 server.use(middlewares)
-server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
 
-server.listen(port, () => {
-    console.log('JSON Server is running',port)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
 
